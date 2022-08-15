ee0606c94c920f3e6b15b5d5f1b83bfaf9c9b045
Damian Tarnawsky <damian@ionic.io>
2022-08-15 13:29:37 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ApplePaymentPass } from '@ionic-enterprise/apple-payment-pass/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ApplePaymentPass],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApplePaymentPass, RequestTemplate } from '@ionic-enterprise/apple-payment-pass/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private paypass: ApplePaymentPass) { }
 
+  async addCardToWallet() {
+    try {
+      const response = await this.paypass.addPaymentPass({
+        headers: undefined,
+        url: undefined,
+        method: 'POST',
+        body: undefined,
+        card: undefined
+      });
+    } catch (e) {
+      if (e.type === 'ServerError') {
+        console.error(`Server Responded with status: ${e.statusCode} and body ${e.body}`);
+      }
+    }
+  }
 }
